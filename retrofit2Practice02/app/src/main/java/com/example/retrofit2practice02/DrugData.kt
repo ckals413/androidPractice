ff02d75b356a7f4049d5ab02049b9c7f58046d75
Chamin Im <ckals413@naver.com>
2024-07-19 14:01:41 +0900

    FEAT: 의존성 추가 및 데이터 클래스 생성

---
+++ retrofit2Practice02/app/src/main/java/com/example/retrofit2practice02/DrugData.kt
@@ -0,0 +1,59 @@
+package com.example.retrofit2practice02
+
+import com.google.gson.annotations.SerializedName
+
+data class DrugData(
+    @SerializedName("body")
+    val body: Body?,
+    @SerializedName("header")
+    val header: Header?
+) {
+    data class Body(
+        @SerializedName("items")
+        val items: List<Item?>?,
+        @SerializedName("numOfRows")
+        val numOfRows: Int?,
+        @SerializedName("pageNo")
+        val pageNo: Int?,
+        @SerializedName("totalCount")
+        val totalCount: Int?
+    ) {
+        data class Item(
+            @SerializedName("atpnQesitm")
+            val atpnQesitm: String?,
+            @SerializedName("atpnWarnQesitm")
+            val atpnWarnQesitm: String?,
+            @SerializedName("bizrno")
+            val bizrno: String?,
+            @SerializedName("depositMethodQesitm")
+            val depositMethodQesitm: String?,
+            @SerializedName("efcyQesitm")
+            val efcyQesitm: String?,
+            @SerializedName("entpName")
+            val entpName: String?,
+            @SerializedName("intrcQesitm")
+            val intrcQesitm: String?,
+            @SerializedName("itemImage")
+            val itemImage: String?,
+            @SerializedName("itemName")
+            val itemName: String?,
+            @SerializedName("itemSeq")
+            val itemSeq: String?,
+            @SerializedName("openDe")
+            val openDe: String?,
+            @SerializedName("seQesitm")
+            val seQesitm: String?,
+            @SerializedName("updateDe")
+            val updateDe: String?,
+            @SerializedName("useMethodQesitm")
+            val useMethodQesitm: String?
+        )
+    }
+
+    data class Header(
+        @SerializedName("resultCode")
+        val resultCode: String?,
+        @SerializedName("resultMsg")
+        val resultMsg: String?
+    )
+}
